f51748b1626ebd6258b313aa8bd8ae0d6d29d7fe
ModiLogist <ModiLogist@Outlook.com>
2023-09-24 18:02:00 +0200

    Updated source and CMake

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,16 @@
+#include "Hooks.h"
+#include "SKSE/Interfaces.h"
+
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+    
     SKSE::Init(skse);
 
-    // Once all plugins and mods are loaded, then the ~ console is ready and can
-    // be printed to
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
         if (message->type == SKSE::MessagingInterface::kDataLoaded)
-            RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
+        {
+            Hooks::Install();
+        }
+            
     });
 
     return true;
