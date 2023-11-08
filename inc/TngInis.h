9863c7b29abbc7de5e2e2d1f41f7823a872f3615
ModiLogist <ModiLogist@Outlook.com>
2023-11-08 18:36:21 +0100

    Added loading code for different ini options.

--- inc/TngInis.h
+++ inc/TngInis.h
@@ -4,13 +4,41 @@ class TngInis : public Singleton<TngInis> {
   private:
     // TNG stuff
     inline static std::string cTngInisPath{R"(.\Data\SKSE\Plugins\TNG)"};
+    inline static constexpr const char cDelimChar{'~'};
+    inline static constexpr const char cColonChar{':'};
+
     inline static constexpr std::string cTngIniEnding{"TNG.ini"};
     inline static constexpr const char* cSkinSection{"Skin"};
     inline static constexpr const char* cSkinMod{"SkinMod"};
+    inline static constexpr const char* cSkinRecord{"SkinRecord"};
+
+    inline static constexpr const char* cArmorSection{"Armor"};
+    inline static constexpr const char* cRevealingMod{"RevealingMod"};
+    inline static constexpr const char* cRevealingRecord{"RevealingRecord"};
+    inline static constexpr const char* cCoveringRecord{"CoveringRecord"};
+
+    inline static constexpr const char* cSwapSection{"SwapSlot"};
+    inline static constexpr const char* cSwapMod{"SwapSlotMod"};
+
+    static void LoadModRecodPairs(std::list<CSimpleIniA::Entry> aModRecords, std::set<std::pair<std::string_view, RE::FormID>> aField);
+
     static bool IsTngIni(const std::string aFileName) noexcept;
     static void LoadSkinMods(CSimpleIniA* aFile) noexcept;
+    static void LoadSkinRecords(CSimpleIniA* aFile) noexcept;
+
+    static void LoadRevealingMods(CSimpleIniA* aFile) noexcept;
+    static void LoadRevealingRecords(CSimpleIniA* aFile) noexcept;
+    static void LoadCoveringRecords(CSimpleIniA* aFile) noexcept;
+
+    static void LoadSwapMods(CSimpleIniA* aFile) noexcept;
 
   public:
     inline static std::set<std::string_view> fSkinMods;
+    inline static std::set<std::pair<std::string_view, RE::FormID>> fSingleSkinIDs;
+    inline static std::set<std::string_view> fRevealingMods;
+    inline static std::set<std::pair<std::string_view, RE::FormID>> fSingleRevealingIDs;
+    inline static std::set<std::pair<std::string_view, RE::FormID>> fSingleCoveringIDs;
+    inline static std::set<std::pair<std::string_view, RE::BGSBipedObjectForm::BipedObjectSlot>> fSwapMods;
+
     static void LoadTngInis() noexcept;
 };
--- src/TngInis.cpp
+++ src/TngInis.cpp
@@ -1,5 +1,15 @@
 #include <TngInis.h>
 
+void TngInis::LoadModRecodPairs(std::list<CSimpleIniA::Entry> aModRecords, std::set<std::pair<std::string_view, RE::FormID>> aField) {
+  for (const auto& lEntry : aModRecords) {
+    const std::string_view lModRecord = std::basic_string_view(lEntry.pItem);
+    const size_t lSepLoc = lModRecord.find(cDelimChar);
+    const RE::FormID lID = std::strtol(lModRecord.substr(0, lSepLoc).data(), nullptr, 0);
+    const std::string_view lModName = lModRecord.substr(lSepLoc + 1);
+    aField.insert(std::make_pair(lModName, lID));
+  }
+}
+
 bool TngInis::IsTngIni(const std::string aFileName) noexcept {
   if (aFileName.size() < cTngIniEnding.size()) return false;
   return std::equal(cTngIniEnding.rbegin(), cTngIniEnding.rend(), aFileName.rbegin());
@@ -12,7 +22,16 @@ void TngInis::LoadTngInis() noexcept {
       aIni.SetUnicode();
       aIni.SetMultiKey();
       aIni.LoadFile(entry.path().string().c_str());
-      if (aIni.SectionExists(cSkinSection)) LoadSkinMods(&aIni);
+      if (aIni.SectionExists(cSkinSection)) {
+        if (aIni.KeyExists(cSkinSection, cSkinMod)) LoadSkinMods(&aIni);
+        if (aIni.KeyExists(cSkinSection, cSkinRecord)) LoadSkinRecords(&aIni);
+      }
+      if (aIni.SectionExists(cArmorSection)) {
+        if (aIni.KeyExists(cArmorSection, cRevealingMod)) LoadRevealingMods(&aIni);
+        if (aIni.KeyExists(cArmorSection, cRevealingRecord)) LoadRevealingRecords(&aIni);
+        if (aIni.KeyExists(cArmorSection, cCoveringRecord)) LoadCoveringRecords(&aIni);
+      }
+      if (aIni.SectionExists(cSwapSection)) LoadSwapMods(&aIni);
     }
   }
 }
@@ -20,8 +39,48 @@ void TngInis::LoadTngInis() noexcept {
 void TngInis::LoadSkinMods(CSimpleIniA* aFile) noexcept {
   std::list<CSimpleIniA::Entry> lMods;
   aFile->GetAllValues(cSkinSection, cSkinMod, lMods);
-  for (const auto& aMod : lMods) {
-    const auto lModName = std::basic_string_view(aMod.pItem);
+  for (const auto& lMod : lMods) {
+    const auto lModName = std::basic_string_view(lMod.pItem);
     fSkinMods.insert(lModName);
+  }
+}
+
+void TngInis::LoadSkinRecords(CSimpleIniA* aFile) noexcept {
+  std::list<CSimpleIniA::Entry> lModRecords;
+  aFile->GetAllValues(cSkinSection, cSkinRecord, lModRecords);
+  LoadModRecodPairs(lModRecords, fSingleSkinIDs);
+}
+
+void TngInis::LoadRevealingMods(CSimpleIniA* aFile) noexcept {
+  std::list<CSimpleIniA::Entry> lMods;
+  aFile->GetAllValues(cArmorSection, cRevealingMod, lMods);
+  for (const auto& lMod : lMods) {
+    const auto lModName = std::basic_string_view(lMod.pItem);
+    fRevealingMods.insert(lModName);
+  }
+}
+
+void TngInis::LoadRevealingRecords(CSimpleIniA* aFile) noexcept {
+  std::list<CSimpleIniA::Entry> lModRecords;
+  aFile->GetAllValues(cArmorSection, cRevealingMod, lModRecords);
+  LoadModRecodPairs(lModRecords, fSingleRevealingIDs);
+}
+
+void TngInis::LoadCoveringRecords(CSimpleIniA* aFile) noexcept {
+  std::list<CSimpleIniA::Entry> lModRecords;
+  aFile->GetAllValues(cArmorSection, cCoveringRecord, lModRecords);
+  LoadModRecodPairs(lModRecords, fSingleCoveringIDs);
+}
+
+void TngInis::LoadSwapMods(CSimpleIniA* aFile) noexcept {
+  std::list<CSimpleIniA::Entry> lEntries;
+  aFile->GetAllValues(cSwapSection, cSwapMod, lEntries);
+  for (const auto& lEntry : lEntries) {
+    const std::string_view lModSwap = std::basic_string_view(lEntry.pItem);
+    const size_t lSepLoc = lModSwap.find(cColonChar);
+    const std::string_view lModName = lModSwap.substr(0, lSepLoc);
+    int lSlotInt = (int) std::pow(2,std::strtol(lModSwap.substr(lSepLoc + 1).data(), nullptr, 0)-30);    
+    RE::BGSBipedObjectForm::BipedObjectSlot lID = static_cast<RE::BIPED_MODEL::BipedObjectSlot>(lSlotInt);
+    fSwapMods.insert(std::make_pair(lModName, lID));
   }
 }
