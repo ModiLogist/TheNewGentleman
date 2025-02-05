b56832ebb083147d3fd19dac74a6fe36f1f7b107
ModiLogist <ModiLogist@Outlook.com>
2025-02-05 19:31:27 +0100

    When they can be friends, encapsulate

--- inc/Events.h
+++ inc/Events.h
@@ -4,23 +4,21 @@ class Events : public RE::BSTEventSink<RE::TESObjectLoadedEvent>, public RE::BST
   public:
     static void RegisterEvents();
 
+    friend class Papyrus;
+
   protected:
     RE::BSEventNotifyControl ProcessEvent(const RE::TESEquipEvent* event, RE::BSTEventSource<RE::TESEquipEvent>*) override;
     RE::BSEventNotifyControl ProcessEvent(const RE::TESObjectLoadedEvent* event, RE::BSTEventSource<RE::TESObjectLoadedEvent>*) override;
     RE::BSEventNotifyControl ProcessEvent(const RE::TESSwitchRaceCompleteEvent* event, RE::BSTEventSource<RE::TESSwitchRaceCompleteEvent>*) override;
 
-  public:
+  private:
     static void DoChecks(RE::Actor* actor, RE::TESObjectARMO* armor = nullptr, bool isEquipped = false);
     static int GetNPCAutoAddon(RE::TESNPC* npc);
-
-  private:
     static void CheckCovering(RE::Actor* actor, RE::TESObjectARMO* armor, bool isEquipped);
     static RE::TESObjectARMO* GetCoveringItem(RE::Actor* actor, RE::TESObjectARMO* armor);
     static bool NeedsCover(RE::Actor* actor);
     static RE::TESBoundObject* ForceTngCover(RE::Actor* actor, bool ifUpdate);
     static void CheckForAddons(RE::Actor* actor);
-
-    
 
     inline static bool showErrMessage;
     inline static std::map<RE::FormID, RE::TESObjectARMO*> oldSkins;
