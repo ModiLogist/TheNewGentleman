fdd84fb1a02bda353c28ee8350228402b4c3442a
ModiLogist <ModiLogist@Outlook.com>
2025-02-24 15:43:33 +0100

    Keywords mismatching fixed

--- inc/Tng.h
+++ inc/Tng.h
@@ -57,8 +57,8 @@ class Tng : public Singleton<Tng> {
     enum eKeywords {
       kyProcessed,
       kyReady,
-      kyIgnored,
       kyPreProcessed,
+      kyIgnored,
       kyExcluded,
       kyTngSkin,
       kySkinWP,
