1e50cf291da89e7cae0487dfc870028c1b055a59
ModiLogist <ModiLogist@Outlook.com>
2025-04-25 17:51:30 +0200

    Not interested in the events to unload actors!

--- src/SEEvents.cpp
+++ src/SEEvents.cpp
@@ -22,8 +22,7 @@ RE::BSEventNotifyControl SEEvents::ProcessEvent(const RE::TESEquipEvent* event, 
 }
 
 RE::BSEventNotifyControl SEEvents::ProcessEvent(const RE::TESObjectLoadedEvent* event, RE::BSTEventSource<RE::TESObjectLoadedEvent>*) {
-  if (!event) return RE::BSEventNotifyControl::kContinue;
-
+  if (!event || !event->loaded) return RE::BSEventNotifyControl::kContinue;
   if (const auto actor = RE::TESForm::LookupByID<RE::Actor>(event->formID); actor) core->UpdateActor(actor);
   return RE::BSEventNotifyControl::kContinue;
 }
